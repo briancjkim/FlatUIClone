af34640ade743e0144a8d3cb3404ebc02635baff
brian <briancjkim91@gmail.com>
2019-05-16 11:06:59 +1000

    color helper to generate colors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,17 @@
 import React from "react";
 import seedColors from "./seedColors";
 import Pallet from "./Pallet";
+import { generatePallet } from "./colorHelpers";
 
-function App() {
-  return (
-    <div className="App">
-      <Pallet {...seedColors[4]} />
-    </div>
-  );
+class App extends React.Component {
+  render() {
+    console.log(generatePallet(seedColors[4]));
+    return (
+      <div className="App">
+        <Pallet {...seedColors[4]} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/ColorBox.js
+++ src/ColorBox.js
@@ -3,11 +3,38 @@ import { CopyToClipboard } from "react-copy-to-clipboard";
 import "./ColorBox.css";
 
 export default class ColorBox extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      copied: false
+    };
+    this.handleCopy = this.handleCopy.bind(this);
+  }
+  handleCopy() {
+    this.setState(
+      {
+        copied: true
+      },
+      () =>
+        setTimeout(() => {
+          this.setState({ copied: false });
+        }, 1500)
+    );
+  }
   render() {
     const { name, background } = this.props;
+    const { copied } = this.state;
     return (
-      <CopyToClipboard text={background}>
+      <CopyToClipboard text={background} onCopy={this.handleCopy}>
         <div className="Colorbox" style={{ background }}>
+          <div
+            className={`copy-overlay ${copied && "show"}`}
+            style={{ background }}
+          />
+          <div className={`copy-msg ${copied && "show"}`}>
+            <h1>Copied</h1>
+            <p>{background}</p>
+          </div>
           <div className="copy-container">
             <div className="box-content">
               <span>{name}</span>
